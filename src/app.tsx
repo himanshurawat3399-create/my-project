292765b7aeb3fe23b3d91ef85352fe786d8331bd
himanshurawat3399-create <himanshurawat3399@gmail.com>
2025-09-08 18:51:02 +0530

    component should start with UPPERCASE character

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,22 @@
 import "./app.css";
-import { ProfileCard } from "./components/ProfileCard.tsx";
+import { ToDoList } from "./components/ToDoList.tsx";
+
 
 export function App() {
   return (
-      <div className="flex intems-center justify-center min-h-screen bg gray-100">
-    <ProfileCard
-      user={{
-        name: "Sophie Bennett",
-        title: "Product Designer who focuses on simplicity & usability.",
-        followers: 312,
-        likes: 48,
-        photo: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1200&auto=format&fit=crop",
-      }}
-    />
+    //   <div className="flex intems-center justify-center min-h-screen bg gray-100">
+    // <ProfileCard
+    //   user={{
+    //     name: "Sophie Bennett",
+    //     title: "Product Designer who focuses on simplicity & usability.",
+    //     followers: 312,
+    //     likes: 48,
+    //     photo: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1200&auto=format&fit=crop",
+    //   }}
+    // />
+    //   </div>
+      <div>
+      <ToDoList/>
       </div>
   );
 }
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,11 @@
+export type ToDoListProps = {
+
+}
+
+export function ToDoList() {
+    return (
+        <div>
+
+        </div>
+    );
+}
